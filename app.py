fcaa88766b13697a1243de7f04bd6f9d63ee4a3f
Suganya <suganya@neosme.com>
2024-10-24 10:38:45 +0530

    added cors

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.stock_profile_api import router as stock_profile_router  # Import the router
 
 from api.search_stock import router as search_stock
 
 # Initialize the FastAPI app
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # Your local development URL
+    "https://riskprotec-suganya-mariasamy-riskprotec.vercel.app"  # Your deployed backend URL
+]
+
+# Add CORS middleware to the app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 
 # Include the stock API router
 app.include_router(stock_profile_router)
@@ -14,3 +31,5 @@ app.include_router(search_stock)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Stock Profile API!"}
+
+
